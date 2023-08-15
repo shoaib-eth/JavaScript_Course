9d23c234bccbbe65b8e0efa0030577915f99644a
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-08-15 16:50:18 +0530

    New Files Addes

---
+++ 03_Objects/01_basicObj.js
@@ -0,0 +1,68 @@
+const person = {
+  name: "Shoaib",
+  course: "Computer Science Engineering",
+  email: "shoaib@gmail.com",
+  address: {
+    street: "New Colony",
+    state: "MP",
+    country: "India",
+  },
+};
+
+// Accessing Object Properties by Dot Notation
+console.log(person.name);
+console.log(person.email);
+console.log(person.address.country);
+
+// Accessing Object Properties by Bracket Notation
+console.log(person["name"]);
+console.log(person["email"]);
+console.log(person["address"]["country"]);
+// Note - > for this type of words `email address`, in which there are space between two words always use bracket notation.
+
+// Modifying Object Properties
+person.name = "Aman";
+person.course = "Mechanical Engineering";
+
+// Now accessing modified object properties
+console.log(person.name);
+console.log(person.course);
+console.log(person.address.country);
+
+// Adding and Deleting Properties in Object
+person.gmail = "shoaib12@gmail.com";
+delete person.email;
+
+// sayHello Function Inside the Object
+const person1 = {
+  name: "Shoaib",
+  course: "Computer Science Engineering",
+  email: "shoaib@gmail.com",
+  address: {
+    street: "New Colony",
+    state: "MP",
+    country: "India",
+  },
+  sayHello: function () {
+    console.log(`Hello, My Name is ${person1.name}`);
+  },
+};
+
+// Calling function
+person1.sayHello();
+
+// Now Creating Array Inside Object
+const person2 = {
+  name: "Shoaib",
+  course: "Computer Science Engineering",
+  hobbies: ["Designing", "Cricket", "Travelling"],
+  email: "shoaib@gmail.com",
+  address: {
+    street: "New Colony",
+    state: "MP",
+    country: "India",
+  },
+};
+
+console.log(person2.hobbies[2]);
+// person2.hobbies[2] - > accessing element of 2nd index in the array.
---
+++ 03_Objects/02_IteratingObj.js
@@ -0,0 +1,29 @@
+// for-in loop
+const obj = { a: 1, b: 2, c: 3 };
+
+for (let key in obj) {
+  console.log(key + " : " + obj[key]);
+}
+
+// Object.keys() Method
+// for-of loop method
+const obj1 = { d: 4, e: 5, f: 6 };
+
+for (let key1 of Object.keys(obj1)) {
+  console.log(key1 + " : " + obj1[key1]);
+}
+
+// Object.entries() Method
+const obj2 = { g: 7, h: 8, i: 9 };
+
+for (let [key2, value] of Object.entries(obj2)) {
+  console.log(key2 + " : " + value);
+}
+
+// Object.values() Method
+const obj3 = { j: 10, k: 11, l: 12 };
+const values = Object.values(obj3);
+
+for (let value of values) {
+  console.log(value);
+}
