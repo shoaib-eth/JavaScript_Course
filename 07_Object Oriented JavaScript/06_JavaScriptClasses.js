f30741c4b765df9ab824928547a3974b08dc7806
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-09-09 16:46:22 +0530

    New Files Added

--- 07_Object Oriented JavaScript/06_JavaScriptClasses.js
+++ 07_Object Oriented JavaScript/06_JavaScriptClasses.js
@@ -54,4 +54,51 @@ class pen {
 }
 
 const pen1 = new pen("Marker", "Blue", "1$");
-console.log(pen1);
+console.log(pen1); // Output - > pen {name: 'Marker', color: 'Blue', price: '1$'}
+
+// Example - > 4
+class Chair {
+  constructor(
+    color,
+    seatHeight,
+    recliningAngle,
+    backSupport,
+    headSupport,
+    padding,
+    armRests,
+    seatSize,
+    isHeightAdjustable,
+    isMovable
+  ) {
+    this.color = color;
+    this.seatHeight = seatHeight;
+    this.recliningAngle = recliningAngle;
+    this.backSupport = backSupport;
+    this.headSupport = headSupport;
+    this.padding = padding;
+    this.armRests = armRests;
+    this.seatSize = seatSize;
+    this.isHeightAdjustable = isHeightAdjustable;
+    this.isMovable = isMovable;
+  }
+
+  adjustableHeight() {}
+  adjustAngle() {}
+  moveChair() {}
+}
+
+const newChair = new Chair(
+  "Blue",
+  "25 inch",
+  "20 deg",
+  true,
+  false,
+  "3 inch",
+  true,
+  "16 inch",
+  false,
+  false
+);
+
+console.dir("Chair Prototype", Chair);
+console.log("Chair Object", newChair);
