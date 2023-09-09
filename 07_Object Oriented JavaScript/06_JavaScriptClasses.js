35e46671ab5f9db39b310da8495b4f7b4c8794eb
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-09-09 16:48:09 +0530

    New Files Added

--- 07_Object Oriented JavaScript/06_JavaScriptClasses.js
+++ 07_Object Oriented JavaScript/06_JavaScriptClasses.js
@@ -102,3 +102,46 @@ const newChair = new Chair(
 
 console.dir("Chair Prototype", Chair);
 console.log("Chair Object", newChair);
+
+// Example - > 5
+class OfficeChair extends Chair {
+  constructor(color, isHeightAdjustable, seatHeight, recliningAngle) {
+    super();
+    this.type = "Office Chair";
+    this.color = color;
+    this.isHeightAdjustable = isHeightAdjustable;
+    this.seatHeight = seatHeight;
+    this.recliningAngle = recliningAngle;
+    this.isMovable = true;
+  }
+
+  adjustableHeight(height) {
+    if (height > this.seatHeight) {
+      console.log(`Chair height changed to ${height}`);
+    } else {
+      console.log(
+        `Height cannot be decreased more than the seat height ${this.seatHeight}`
+      );
+    }
+  }
+
+  adjustAngle(angle) {
+    if (angle >= this.recliningAngle) {
+      console.log(`Chair angle changed to ${angle}`);
+    } else {
+      console.log(
+        `Angle cannot be decreased more than the min reclining angle ${this.recliningAngle}`
+      );
+    }
+  }
+
+  moveChair(x, y) {
+    console.log(`Chair moved to co-ordinates = (${x}, ${y})`);
+  }
+}
+
+const newOfficeChair = new OfficeChair("Red", true, 30, 30);
+
+console.log(newOfficeChair.adjustableHeight(31));
+console.log(newOfficeChair.adjustAngle(40));
+console.log(newOfficeChair.moveChair(10, 20));
