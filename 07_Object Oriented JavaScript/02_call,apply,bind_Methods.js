2ba46c26c38ee6f5272e22efe32cd314cd5b16ed
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-10-06 17:09:26 +0530

    Initial commit

--- 07_Object Oriented JavaScript/02_call,apply,bind_Methods.js
+++ 07_Object Oriented JavaScript/02_call,apply,bind_Methods.js
@@ -63,3 +63,24 @@ function definePrice(price) {
 
 const newBrand = new setBrand(`Brand1`);
 const newCarPrice = new definePrice(100000);
+
+// Example - > 2 : apply() function
+function Car(type, fuelType) {
+  this.type = type;
+  this.fuelType = fuelType;
+}
+
+function setBrand(brand) {
+  Car.apply(this, ["convertible", "petrol"]); //Syntax with array literal
+  this.brand = brand;
+  console.log(`Car details = `, this);
+}
+
+function definePrice(price) {
+  Car.apply(this, new Array("convertible", "diesel")); //Syntax with array object construction
+  this.price = price;
+  console.log(`Car details = `, this);
+}
+
+const newBrand1 = new setBrand("Brand1");
+const newCarPrice1 = new definePrice(100000);
