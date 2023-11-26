055c77c91e31cbb860d867ce2d4e7a16a09f1027
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-11-26 18:03:39 +0530

    Initial commit

---
+++ JavaScript Mastery/Uint - 01/Chapter - 01/Variables.js
@@ -0,0 +1,16 @@
+// Question - 1 :  Declare a variable using var and another using let. Assign values to both variables and log them to the console.
+
+// Declare a variable called 'myVarNum' using the 'var' keyword and assign it the value 21.
+var myVarNum = 21;
+
+// Declare a variable called 'myLetNum' using the 'let' keyword and assign it the value 22.
+let myLetNum = 22;
+
+// Log the value of 'myVarNum' to the console.
+console.log(myVarNum);
+
+// Log the value of 'myLetNum' to the console.
+console.log(myLetNum);
+
+// ------------------------------------------------------------------------//
+
