5a1c8eeb5a06d17473c2cb9e41cad82946cd858e
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-11-26 17:53:32 +0530

    Initial commit

--- JavaScript Mastery/Uint - 01/Chapter - 01/hello.js
+++ JavaScript Mastery/Uint - 01/Chapter - 01/hello.js
@@ -3,3 +3,5 @@
 // The information to be output is passed to the function as an argument.
 
 console.log("Hello World"); // Here, the string 'Hello World' is passed to the console.log() function. When this line of code is executed, 'Hello World' will be printed to the web console.
+
+console.log("Nice to meet you!");
