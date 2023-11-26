e15ceca5b2116d419a52d3364b2b8d0f6c3b2554
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-11-26 17:55:30 +0530

    Initial commit

--- 01_Basics of JavaScript/01_helloworld.js
+++ 01_Basics of JavaScript/01_helloworld.js
@@ -1,3 +1,7 @@
-console.log("Hello World!");
-// console.log is used print something on the screen
+// This is a single line of JavaScript code that uses the console.log() function.
+// The console.log() function is a built-in function in JavaScript which outputs information to the web console.
+// The information to be output is passed to the function as an argument.
 
+console.log("Hello World"); // Here, the string 'Hello World' is passed to the console.log() function. When this line of code is executed, 'Hello World' will be printed to the web console.
+
+console.log("Nice to meet you!");
