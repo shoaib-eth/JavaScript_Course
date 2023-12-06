8633ceb51cc03a726fac4e7dc93a6370510bff7f
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-12-06 18:53:50 +0530

    Deleted JavaScript Mastery Folder

--- JavaScript Mastery/Uint - 01/Chapter - 01/hello.js
+++
@@ -1,9 +0,0 @@
-// This is a single line of JavaScript code that uses the console.log() function.
-// The console.log() function is a built-in function in JavaScript which outputs information to the web console.
-// The information to be output is passed to the function as an argument.
-
-console.log("Hello World"); // Here, the string 'Hello World' is passed to the console.log() function. When this line of code is executed, 'Hello World' will be printed to the web console.
-
-console.log("Nice to meet you!");
-
-// The console.log() function can also be used to output the value of a variable.
