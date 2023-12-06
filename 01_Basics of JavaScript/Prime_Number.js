1c3e343ccc8e99527ba74600348d8206ff79c959
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-12-06 19:00:22 +0530

    Initial commit

--- 01_Basics of JavaScript/Prime_Number.js
+++ 01_Basics of JavaScript/Prime_Number.js
@@ -1,16 +1,21 @@
 function isPrime(num) {
-  if (num <= 1) {
-    return false;
-  }
-
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) {
-      return false;
+    if (num <= 1) {
+        return false;
     }
-  }
 
-  return true;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
 }
+
+const userInput = prompt("Enter a number:");
+const number = parseInt(userInput);
+
+console.log(isPrime(number));
 
 // Example usage
 console.log(isPrime(7)); // Output: true
