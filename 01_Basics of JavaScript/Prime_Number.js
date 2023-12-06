4131b5c4f5422b22bf6f67b8859ff92a18cb413f
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-12-06 18:59:21 +0530

    Initial commit

---
+++ 01_Basics of JavaScript/Prime_Number.js
@@ -0,0 +1,17 @@
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Example usage
+console.log(isPrime(7)); // Output: true
+console.log(isPrime(10)); // Output: false
