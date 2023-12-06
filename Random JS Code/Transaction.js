e361db11f5340d1a8e6cf9c36f2f40731400c7ed
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-12-06 18:57:04 +0530

    Initial commit

---
+++ Random JS Code/Transaction.js
@@ -0,0 +1,11 @@
+
+fetch('https://example.com/transaction-details')
+  .then(response => response.json())
+  .then(data => {
+    // Process the transaction details
+    // For example, you can display them in an alert
+    alert(JSON.stringify(data));
+  })
+  .catch(error => {
+    console.error('Error fetching transaction details:', error);
+  });
