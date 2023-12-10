09467397c122b8737958eecbffcd4f2228289402
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-12-10 17:39:59 +0530

    Initial commit

--- 01_Basics of JavaScript/Calculator.js
+++ 01_Basics of JavaScript/Calculator.js
@@ -1,26 +1,38 @@
 function add(a, b) {
-  return a + b;
+    return a + b;
 }
 
 function subtract(a, b) {
-  return a - b;
+    return a - b;
 }
 
 function multiply(a, b) {
-  return a * b;
+    return a * b;
 }
 
 function divide(a, b) {
-  if (b === 0) {
-    throw new Error("Cannot divide by zero");
-  }
-  return a / b;
+    if (b === 0) {
+        throw new Error("Cannot divide by zero");
+    }
+    return a / b;
 }
 
 const num1 = parseFloat(prompt("Enter the first number:"));
 const num2 = parseFloat(prompt("Enter the second number:"));
 
-console.log("Addition:", add(num1, num2));
-console.log("Subtraction:", subtract(num1, num2));
-console.log("Multiplication:", multiply(num1, num2));
-console.log("Division:", divide(num1, num2));
+const operation = parseInt(prompt("Choose an operation:\n1. Addition\n2. Subtraction\n3. Division\n4. Multiplication"));
+
+let result;
+if (operation === 1) {
+    result = add(num1, num2);
+} else if (operation === 2) {
+    result = subtract(num1, num2);
+} else if (operation === 3) {
+    result = divide(num1, num2);
+} else if (operation === 4) {
+    result = multiply(num1, num2);
+} else {
+    console.log("Invalid operation");
+}
+
+console.log("Result:", result);
