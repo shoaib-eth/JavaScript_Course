8ab9e8ded693c8d233d39660b213f3cadb900561
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-12-10 17:36:58 +0530

    Initial commit

--- 01_Basics of JavaScript/Calculator.js
+++ 01_Basics of JavaScript/Calculator.js
@@ -1,20 +1,20 @@
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
 function subtract(a, b) {
-    return a - b;
+  return a - b;
 }
 
 function multiply(a, b) {
-    return a * b;
+  return a * b;
 }
 
 function divide(a, b) {
-    if (b === 0) {
-        throw new Error("Cannot divide by zero");
-    }
-    return a / b;
+  if (b === 0) {
+    throw new Error("Cannot divide by zero");
+  }
+  return a / b;
 }
 
 const num1 = parseFloat(prompt("Enter the first number:"));
