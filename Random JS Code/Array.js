30ef0105f8b9fe17c5ad9efb42ce6f2599fb0d3a
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-12-17 18:33:11 +0530

    Initial commit

--- Random JS Code/Array.js
+++ Random JS Code/Array.js
@@ -1,15 +1,15 @@
 // Creating an array
-const fruits = ['apple', 'banana', 'cherry', 'date'];
+const fruits = ["apple", "banana", "cherry", "date"];
 
 // Accessing array elements
 console.log(fruits[0]); // Output: apple
 
 // Modifying array elements
-fruits[1] = 'grape';
+fruits[1] = "grape";
 console.log(fruits); // Output: ['apple', 'grape', 'cherry', 'date']
 
 // Adding elements to the end of an array
-fruits.push('elderberry');
+fruits.push("elderberry");
 console.log(fruits); // Output: ['apple', 'grape', 'cherry', 'date', 'elderberry']
 
 // Removing the last element from an array
@@ -17,7 +17,7 @@ fruits.pop();
 console.log(fruits); // Output: ['apple', 'grape', 'cherry', 'date']
 
 // Adding elements to the beginning of an array
-fruits.unshift('apricot');
+fruits.unshift("apricot");
 console.log(fruits); // Output: ['apricot', 'apple', 'grape', 'cherry', 'date']
 
 // Removing the first element from an array
@@ -25,7 +25,7 @@ fruits.shift();
 console.log(fruits); // Output: ['apple', 'grape', 'cherry', 'date']
 
 // Finding the index of an element in an array
-const index = fruits.indexOf('cherry');
+const index = fruits.indexOf("cherry");
 console.log(index); // Output: 2
 
 // Removing elements from a specific position in an array
@@ -34,7 +34,7 @@ console.log(fruits); // Output: ['apple', 'date']
 console.log(removedElements); // Output: ['grape', 'cherry']
 
 // Combining arrays
-const moreFruits = ['fig', 'grapefruit'];
+const moreFruits = ["fig", "grapefruit"];
 const combinedArray = fruits.concat(moreFruits);
 console.log(combinedArray); // Output: ['apple', 'date', 'fig', 'grapefruit']
 
@@ -47,17 +47,20 @@ const sortedArray = fruits.sort();
 console.log(sortedArray); // Output: ['apple', 'date']
 
 // Checking if an element exists in an array
-const exists = fruits.includes('apple');
+const exists = fruits.includes("apple");
 console.log(exists); // Output: true
 
 // Mapping over an array
-const mappedArray = fruits.map(fruit => fruit.toUpperCase());
+const mappedArray = fruits.map((fruit) => fruit.toUpperCase());
 console.log(mappedArray); // Output: ['APPLE', 'DATE']
 
 // Filtering an array
-const filteredArray = fruits.filter(fruit => fruit.startsWith('a'));
+const filteredArray = fruits.filter((fruit) => fruit.startsWith("a"));
 console.log(filteredArray); // Output: ['apple']
 
 // Reducing an array
-const reducedValue = fruits.reduce((accumulator, fruit) => accumulator + fruit, '');
+const reducedValue = fruits.reduce(
+  (accumulator, fruit) => accumulator + fruit,
+  ""
+);
 console.log(reducedValue); // Output: 'appledate'
