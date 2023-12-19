a0b546c5b69bff6042a43e4d2f9f39d4bbf52c96
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-12-19 18:18:34 +0530

    Initial commit

--- Random JS Code/Forms.js
+++ Random JS Code/Forms.js
@@ -1,24 +1,24 @@
 // Create a form element
-const form = document.createElement('form');
+const form = document.createElement("form");
 
 // Set form attributes
-form.setAttribute('method', 'POST');
-form.setAttribute('action', '/submit-form');
+form.setAttribute("method", "POST");
+form.setAttribute("action", "/submit-form");
 
 // Create form fields
-const inputName = document.createElement('input');
-inputName.setAttribute('type', 'text');
-inputName.setAttribute('name', 'name');
-inputName.setAttribute('placeholder', 'Enter your name');
+const inputName = document.createElement("input");
+inputName.setAttribute("type", "text");
+inputName.setAttribute("name", "name");
+inputName.setAttribute("placeholder", "Enter your name");
 
-const inputEmail = document.createElement('input');
-inputEmail.setAttribute('type', 'email');
-inputEmail.setAttribute('name', 'email');
-inputEmail.setAttribute('placeholder', 'Enter your email');
+const inputEmail = document.createElement("input");
+inputEmail.setAttribute("type", "email");
+inputEmail.setAttribute("name", "email");
+inputEmail.setAttribute("placeholder", "Enter your email");
 
-const submitButton = document.createElement('button');
-submitButton.setAttribute('type', 'submit');
-submitButton.textContent = 'Submit';
+const submitButton = document.createElement("button");
+submitButton.setAttribute("type", "submit");
+submitButton.textContent = "Submit";
 
 // Append form fields to the form element
 form.appendChild(inputName);
