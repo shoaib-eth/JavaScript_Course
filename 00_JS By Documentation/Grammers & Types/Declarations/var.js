7cca10decc56c2f4b0c8538d26fc55c4e485c5d3
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-12-28 15:18:47 +0530

    Initial commit

---
+++ 00_JS By Documentation/Grammers & Types/Declarations/var.js
@@ -0,0 +1,19 @@
+// Example - 1
+var x = 1;
+
+if (x === 1) {
+  var x = 2;
+  console.log(x);
+}
+
+console.log(x);
+
+// Example - 2 : Different Syntax to declare variable using var
+
+var name1;
+var name1 = value1;
+var name1 = value1, name2 = value2;
+var name1, name2 = value2;
+var name1 = value1, name2, /* …, */ nameN = valueN;
+
+
