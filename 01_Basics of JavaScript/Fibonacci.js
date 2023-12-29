ed75a3c2ef3ad4edfe17d4b2a747ba96ef1ccc97
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-12-29 09:30:11 +0530

    Initial commit

--- 01_Basics of JavaScript/Fibonacci.js
+++ 01_Basics of JavaScript/Fibonacci.js
@@ -1,11 +1,11 @@
 function fibonacciSeries(n) {
-    if (n <= 1) {
-        return [0, 1];
-    } else {
-        const series = fibonacciSeries(n - 1);
-        series.push(series[series.length - 1] + series[series.length - 2]);
-        return series;
-    }
+  if (n <= 1) {
+    return [0, 1];
+  } else {
+    const series = fibonacciSeries(n - 1);
+    series.push(series[series.length - 1] + series[series.length - 2]);
+    return series;
+  }
 }
 
 const n = 10; // Change this value to generate Fibonacci series up to a different number
@@ -13,7 +13,5 @@ const series = fibonacciSeries(n);
 
 console.log("The Fibonacci series up to", n, "is:");
 for (let i = 0; i < series.length; i++) {
-    console.log(series[i]);
+  console.log(series[i]);
 }
-
-
