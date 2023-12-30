8690f4cafdfb5715d44b2a93e1299672a964a5e3
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-12-30 09:11:30 +0530

    Initial commit

--- 01_Basics of JavaScript/08_typeofOperator.js
+++ 01_Basics of JavaScript/08_typeofOperator.js
@@ -7,7 +7,18 @@ console.log(typeof string);
 console.log(typeof bool);
 
 // We can write value of the variables for checking the type
-console.log("After writting value of variable");
+console.log("After writing value of variable");
 console.log(typeof 10);
 console.log(typeof "Hello");
 console.log(typeof true);
+
+const obj = { name: "John", age: 30 };
+const arr = [1, 2, 3, 4, 5];
+const func = function() {
+    console.log("Hello, world!");
+};
+
+console.log(typeof obj);
+console.log(typeof arr);
+console.log(typeof func);
+
