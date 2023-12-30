08d53b5b9bc5cea4705278eee14386a9822f54c0
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-12-30 09:13:14 +0530

    Initial commit

--- 01_Basics of JavaScript/10_StringToNumber.js
+++ 01_Basics of JavaScript/10_StringToNumber.js
@@ -8,7 +8,22 @@ let strFloat = "12.3";
 let numFloat = parseFloat(strFloat);
 console.log(typeof numFloat);
 
-// By Using unary + operato
+// By Using unary + operator
 let str1 = "1234";
 let num1 = +str1;
 console.log(typeof num1);
+
+// Additional examples
+let str2 = "45.67";
+let num2 = parseFloat(str2);
+console.log(typeof num2);
+
+let str3 = "100";
+let num3 = parseInt(str3);
+console.log(typeof num3);
+
+let str4 = "0.123";
+let num4 = +str4;
+console.log(typeof num4);
+
+
