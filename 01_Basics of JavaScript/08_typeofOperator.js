0df4f5f991ca9c96a72b2f3167c4f5d84952bd36
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-12-30 09:11:43 +0530

    Initial commit

--- 01_Basics of JavaScript/08_typeofOperator.js
+++ 01_Basics of JavaScript/08_typeofOperator.js
@@ -14,11 +14,10 @@ console.log(typeof true);
 
 const obj = { name: "John", age: 30 };
 const arr = [1, 2, 3, 4, 5];
-const func = function() {
-    console.log("Hello, world!");
+const func = function () {
+  console.log("Hello, world!");
 };
 
 console.log(typeof obj);
 console.log(typeof arr);
 console.log(typeof func);
-
