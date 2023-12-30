9d652219f994d0d8b7c81bf6477375569db0adbd
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-12-30 09:28:09 +0530

    Initial commit

--- 01_Basics of JavaScript/Area_of_Circel.js
+++ 01_Basics of JavaScript/Area_of_Circel.js
@@ -1,8 +1,16 @@
 // Prompt the user to enter the radius of the circle
 const radius = parseFloat(prompt("Enter the radius of the circle:"));
 
-// Calculate the area of the circle
-const area = Math.PI * Math.pow(radius, 2);
+// Calculate the area of the circle using a function
+function calculateArea(radius) {
+    return Math.PI * Math.pow(radius, 2);
+}
+
+// Call the function and store the result in a variable
+const area = calculateArea(radius);
 
 // Display the result
 console.log(`The area of the circle is: ${area}`);
+
+
+
