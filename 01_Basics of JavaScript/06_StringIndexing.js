ee8d726f8fafcffb01686e40b479326bee91625a
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-12-30 09:09:36 +0530

    Initial commit

--- 01_Basics of JavaScript/06_StringIndexing.js
+++ 01_Basics of JavaScript/06_StringIndexing.js
@@ -5,10 +5,8 @@ console.log(myString[3]);
 console.log(myString[7]);
 console.log(myString[myString.length - 1]);
 
-// Using carAt()
-
 const myName = "Shoaib";
 
-console.log(myName.charAt(1));
-console.log(myName.charAt(4));
-console.log(myName.charAt(myName.length - 1));
+console.log(myName[1]);
+console.log(myName[4]);
+console.log(myName[myName.length - 1]);
