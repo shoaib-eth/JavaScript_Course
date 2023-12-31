68c01f523ea2f90ec8bac6ffbfc119f39769468d
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-12-31 17:46:12 +0530

    Initial commit

--- 03_Objects/04_SpreadOperator.js
+++ 03_Objects/04_SpreadOperator.js
@@ -25,3 +25,11 @@ const person = {
 const newObject = { ...person, Branch: "CSE" };
 
 console.log(newObject);
+
+
+// Combining arrays
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const combinedArray = [...arr1, ...arr2];
+
+console.log(combinedArray);
