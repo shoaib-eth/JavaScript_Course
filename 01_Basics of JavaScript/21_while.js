3c02b216f3e9d61d4ba8bd093e28a451702c6cbc
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-01 18:02:40 +0530

    Initial commit

--- 01_Basics of JavaScript/21_while.js
+++ 01_Basics of JavaScript/21_while.js
@@ -19,3 +19,14 @@ while (x >= 0) {
   console.log(`x: ${x}`);
   x--;
 }
+let biggest = 0;
+let numbers = [5, 10, 2, 8, 3];
+
+while (numbers.length > 0) {
+  let currentNumber = numbers.pop();
+  if (currentNumber > biggest) {
+    biggest = currentNumber;
+  }
+}
+
+console.log(`The biggest number is ${biggest}`);
