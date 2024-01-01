ad7fd99a17ed5a4810506b3f1c6d31b1e9990bbe
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-01 18:05:15 +0530

    Initial commit

--- 01_Basics of JavaScript/22_do-while.js
+++ 01_Basics of JavaScript/22_do-while.js
@@ -1,3 +1,4 @@
+// Example - > 1
 let i = 5;
 
 do {
@@ -5,6 +6,7 @@ do {
   i++;
 } while (i <= 1);
 
+// Example - > 2
 let count = 1;
 
 do {
@@ -12,9 +14,22 @@ do {
   count++;
 } while (count <= 5);
 
+// Example - > 3
 let x = 3;
 
 do {
   console.log(`x: ${x}`);
   x--;
 } while (x >= 0);
+
+// Example - > 4
+let biggest = 0;
+let numbers = [5, 10, 2, 8, 3];
+
+do {
+  let currentNumber = numbers.pop();
+  if (currentNumber > biggest) {
+    biggest = currentNumber;
+  }
+}
+while (numbers.length > 0);
