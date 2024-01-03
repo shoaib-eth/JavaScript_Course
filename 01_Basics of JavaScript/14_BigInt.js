a946ae6d2672f06374b26d4457cafda03ea35ae4
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-03 17:49:15 +0530

    Initial commit

--- 01_Basics of JavaScript/14_BigInt.js
+++ 01_Basics of JavaScript/14_BigInt.js
@@ -7,3 +7,12 @@ const bigNum1 = 98723983745809394859034094n;
 
 let result = bigNum + bigNum1;
 console.log(result);
+
+
+const bigNum2 = 1234567890123456789012345678901234567890n;
+console.log(bigNum);
+
+const bigNum3 = 9876543210987654321098765432109876543210n;
+
+let result1 = bigNum2 * bigNum3;
+console.log(result);
