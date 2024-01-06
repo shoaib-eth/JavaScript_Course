451347e5730306167cef2122cb50c6548f60f38f
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-06 17:59:30 +0530

    Initial commit

--- 01_Basics of JavaScript/19_Nested_if-else.js
+++ 01_Basics of JavaScript/19_Nested_if-else.js
@@ -7,3 +7,20 @@ if (num > 0) {
 } else {
   console.log("Zero");
 }
+
+// Using logical operators to combine conditions
+let temperature = 25;
+let isRaining = true;
+
+if (temperature > 30 && !isRaining) {
+  console.log("It's a hot and sunny day");
+}
+else if (temperature > 30 && isRaining) {
+  console.log("It's a hot day, but it's raining");
+}
+else if (temperature <= 30 && !isRaining) {
+  console.log("It's not raining but the temperature is moderate");
+}
+else {
+  console.log("Don't even ask");
+}
