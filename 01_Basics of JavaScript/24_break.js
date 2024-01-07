71a46af15695424372f19d3edd09b96c8fdf81c3
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-07 17:23:10 +0530

    Initial commit

--- 01_Basics of JavaScript/24_break.js
+++ 01_Basics of JavaScript/24_break.js
@@ -4,3 +4,23 @@ for (let i = 0; i <= 10; i++) {
   }
   console.log(`The value if i is ${i}`);
 }
+
+// Example - > 2
+
+for (let i = 0; i <= 10; i++) {
+  if (i === 5) {
+    break;
+  }
+  console.log(`The value if i is ${i}`);
+}
+
+// Example - > 3
+
+for (let i = 0; i <= 10; i++) {
+  if (i === 5) {
+    break;
+  }
+  console.log(`The value if i is ${i}`);
+}
+
+
