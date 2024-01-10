3a37430440a484b7d5dcd9139f3b7491055ad94f
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-10 11:19:15 +0530

    Initial commit

--- Practice/Practice.js
+++ Practice/Practice.js
@@ -1,15 +1,1 @@
-function foo() {
-  var x = 1;
-
-  function bar() {
-    var y = 2;
-
-    console.log(x);
-    console.log(y);
-  }
-  bar();
-
-  console.log(x);
-  console.log(y);
-}
-foo();
+console.log("I'm JavaScript!");
