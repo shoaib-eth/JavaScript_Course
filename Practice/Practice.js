7965e1ef86a6ef46950928b8d46705181767cbe8
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-11 16:46:59 +0530

    Initial commit

--- Practice/Practice.js
+++ Practice/Practice.js
@@ -1,8 +1,4 @@
-const COLOR_RED = "#F00";
-const COLOR_GREEN = "#0F0";
-const COLOR_BLUE = "#00F";
-const COLOR_ORANGE = "#FF7F00";
+let $ = 3;
+let _ =4;
 
-// ...when we need to pick a color
-let color = COLOR_ORANGE;
-alert(color); // #FF7F00
+console.log($ + _);
