38dbfefbb196bfa9196ea6232b9c7374d8cde105
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-15 13:48:42 +0530

    Initial commit

--- Practice/Practice.js
+++ Practice/Practice.js
@@ -12,3 +12,12 @@ let currentUserName = "John";
 
 console.log(`Our Planest Name is ${ourPlanetName}`);
 console.log(`Current User Name is ${currentUserName}`);
+
+// Example - 3 : Uppercase constants
+
+const BIRTHDAY = "18.04.1982"; // make uppercase?
+
+const AGE = someCode(BIRTHDAY); // make uppercase?
+
+console.log(`My Birthday is ${BIRTHDAY}`);
+console.log(`My Age is ${AGE}`);
