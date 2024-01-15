8e809450162b50760675371fe8c72a7f263d2726
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-15 13:40:36 +0530

    Example - 1

--- Practice/Practice.js
+++ Practice/Practice.js
@@ -1,14 +1,7 @@
-let $ = 3;
-let _ = 4;
-
-console.log($ + _);
-
-// Example - > Working with variables
-
-let admin, name;
+let admin;
+let name;
 
 name = "John";
-
 admin = name;
 
-alert(admin); // "John"
+console.log(admin);
