4eee26e051273b225154b0c84b9d337d09df6c54
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-15 13:44:14 +0530

    Initial commit

--- Practice/Practice.js
+++ Practice/Practice.js
@@ -9,3 +9,6 @@ console.log(admin);
 // Example - 2 : Naming correct variable names
 let ourPlanetName = "Earth";
 let currentUserName = "John";
+
+console.log(`Our Planest Name is ${ourPlanetName}`);
+console.log(`Current User Name is ${currentUserName}`);
