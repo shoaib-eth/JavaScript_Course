9a1c735ee714d9495db3d4a28d480ec90a96b787
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-15 14:17:38 +0530

    Initial commit

--- Modern JavaScript/03_DataTypes.js
+++ Modern JavaScript/03_DataTypes.js
@@ -1,2 +1,11 @@
 console.log("Not a Number" / 2); // NaN, such division is erroneous
 console.log("Not a Number" / 2 + 5); // NaN
+
+// Example - Special numeric values Infinity, -Infinity and NaN
+
+console.log(1 / 0); // Infinity
+console.log(Infinity); // Infinity
+console.log(Infinity + 1); // Infinity
+console.log(Infinity + Infinity); // Infinity
+console.log(Infinity - Infinity); // NaN
+console.log(1 / Infinity); // 0
