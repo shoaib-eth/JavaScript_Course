e16901ddab37f17db3aa3607723f558ac263107a
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-16 12:17:15 +0530

    Initial commit

---
+++ Modern JavaScript/04_Interactions.js
@@ -0,0 +1,28 @@
+// alert
+// alert("Hello World!");
+
+// prompt
+
+// Example - 1
+// let name = prompt("What is your name?");
+// console.log(name);
+
+// Example - 2
+// let age = prompt("How old you are?", 21);
+
+// alert(`You are ${age} years old!`);
+
+// confirm
+
+// Example - 1
+// let isProgrammer = confirm("Are you the programmer?");
+// alert(isProgrammer);
+
+// Task - 1
+
+let name = prompt("What is your name?");
+alert(`Hello! ${name}`);
+
+// Task - 2
+let age = prompt("How old you are?");
+alert(`You are ${age} years old!`);
