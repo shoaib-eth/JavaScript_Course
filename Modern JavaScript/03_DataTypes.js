c9b644b1660248fe8c8181cbd190bac257df15a5
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-16 10:45:45 +0530

    typeof Operator

--- Modern JavaScript/03_DataTypes.js
+++ Modern JavaScript/03_DataTypes.js
@@ -58,7 +58,25 @@ if (isEven % 2 == 0) {
 // Example - `null` and `undefined`
 
 let age = null;
-console.log(age);   // null
+console.log(age); // null
 
 let age2;
-console.log(age2);   // undefined
+console.log(age2); // undefined
+
+let age3 = 18;
+
+age3 = undefined;
+
+console.log(age3); // undefined
+
+// Example - typeof operator
+
+console.log(typeof undefined); // "undefined"
+console.log(typeof 0); // "number"
+console.log(typeof 10n); // "bigint"
+console.log(typeof true); // "boolean"
+console.log(typeof "foo"); // "string"
+console.log(typeof Symbol("id")); // "symbol"
+console.log(typeof Math); // "object"  (1)
+console.log(typeof null); // "object"  (2)
+console.log(typeof alert); // "function"  (3)
