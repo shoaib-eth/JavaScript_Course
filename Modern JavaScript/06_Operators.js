f0f5c62aeca31c507de1a377cf22887267ee416c
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-17 10:26:13 +0530

    Remainder and Exponentiation Operator

--- Modern JavaScript/06_Operators.js
+++ Modern JavaScript/06_Operators.js
@@ -4,3 +4,22 @@
 let x = 1;
 x = -x;
 console.log(x); // -1
+
+// Example - Binary Operators
+// Binary operators are operators that operate on two values.
+
+let a = 1,
+  b = 3;
+console.log(b - a); // 2
+
+// Example - Remainder Operator
+// The remainder operator returns the remainder of the division of two numbers.
+
+console.log(5 % 2); // 1
+console.log(8 % 3); // 2
+
+// Example - Exponentiation Operator
+// The exponentiation operator returns the result of raising the first operand to the power of the second operand.
+
+console.log(2 ** 3); // 8
+console.log(3 ** 2); // 9
