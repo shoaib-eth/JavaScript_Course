6e067faa6d09c20b50baf003b3c2f89413a1334d
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-17 10:02:37 +0530

    Numeric Conversion

--- Modern JavaScript/05_TypeConversions.js
+++ Modern JavaScript/05_TypeConversions.js
@@ -1,7 +1,17 @@
 // Example - String Conversion
 
 let value = true;
-console.log(typeof value);  // boolean
+console.log(typeof value); // boolean
 
 value = String(value);
-console.log(typeof value);  // string
+console.log(typeof value); // string
+
+// Example - Numeric Conversion
+
+console.log("10" / "5"); // 2, strings are converted to numbers
+
+let str = "11082002";
+console.log(typeof str); // string
+
+str = Number(str); // becomes a number 11082002
+console.log(typeof str); // number
