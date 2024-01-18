8536bd4c07c6c475c5fe783236153f6a7001537f
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-18 16:46:40 +0530

    String Comparison

--- Modern JavaScript/07_Comparisons.js
+++ Modern JavaScript/07_Comparisons.js
@@ -4,3 +4,20 @@ let b = 4;
 
 console.log(a > b); // true
 console.log(a < b); // false
+
+// String Comparison
+
+let a1 = "Z";
+let b1 = "A";
+
+console.log(a1 > b1); // true
+console.log(a1 < b1); // false
+
+console.log("Z" > "a"); // false
+console.log("a" > "Z"); // true
+
+console.log("John" > "Alice"); // true
+console.log("John" > "Alice"); // true
+
+console.log("Glow" > "Glee"); // true
+console.log("Bee" > "Be"); // true
