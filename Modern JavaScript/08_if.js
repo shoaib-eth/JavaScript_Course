d356a3051f4f3b60c430ca14e706155ee35c7e08
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-21 16:50:15 +0530

    Initail commit

---
+++ Modern JavaScript/08_if.js
@@ -0,0 +1,11 @@
+// Example - 1
+
+let year = prompt(
+  "In which year was ECMAScript-2015 specification published?",
+  ""
+);
+
+if (year == 2015) {
+  alert("You are right!");
+  alert("You are really smart!");
+}
