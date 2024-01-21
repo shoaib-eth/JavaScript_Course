0973a380750eab7a34bd9baa5112c05aaaab5279
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-21 17:37:37 +0530

    Initial commit

--- Modern JavaScript/08_if.js
+++ Modern JavaScript/08_if.js
@@ -1,6 +1,6 @@
 // Example - 1
 
-/*let year = prompt(
+let year = prompt(
   "In which year was ECMAScript-2015 specification published?",
   ""
 );
@@ -8,11 +8,11 @@
 if (year == 2015) {
   alert("You are right!");
   alert("You are really smart!");
-}*/
+}
 
 // Example - 2
 
-/*let year2 = prompt(
+let year2 = prompt(
   "In which year was ECMAScript-2015 specification published?",
   ""
 );
@@ -21,11 +21,10 @@ if (year2 == 2015) {
   alert("You are right!");
 } else {
   alert("How can you be so wrong?");
-} */
+}
 
 // Example - 3
 
-/*
 let year3 = prompt(
   "In which year was ECMAScript-2015 specification published?",
   ""
@@ -38,11 +37,9 @@ if (year3 > 2015) {
 } else {
   alert("Exactly!");
 }
-*/
 
 // Example - 4 : Conditional Operator '?'
 
-/*
 let accessAllowed;
 let age = prompt("How old are you?", "");
 
@@ -51,20 +48,16 @@ if (age > 18) {
 } else {
   accessAllowed = false;
 }
-*/
 
 // Example - 5 : Terinary Operator '?'
 
-/*
 let ageTernary = ageTernary > 18 ? true : false;
 alert(ageTernary);
 
 let ageTernary2 = ageTernary2 > 18; // without if statement (same as above) (if ageTernary2 > 18 is true then ageTernary2 = true)
-*/
 
 // Example - 6 : Multiple '?'
 
-/*
 let age = prompt("age?", "");
 
 let message =
@@ -77,11 +70,9 @@ let message =
     : "What an unusual age!";
 
 alert(message);
-*/
 
 // By using if-else
 
-/*
 let age = prompt("age?", "");
 
 if (age < 3) {
@@ -93,19 +84,15 @@ if (age < 3) {
 } else {
   alert("What an unusual age!");
 }
-*/
 
 // Example - 7 : Non-traditional use of '?'
 
-/*
 let company = prompt("Which campany created JavaScript?", "");
 
 company == "Netscape" ? alert("Right!") : alert("Wrong.");
-*/
 
 // By using if-else
 
-/*
 let company = prompt("Which company created JavaScript?", "");
 
 if (company == "Netscape") {
@@ -113,21 +100,17 @@ if (company == "Netscape") {
 } else {
   alert("Wrong");
 }
-*/
 
 // Tasks
 
 // Task - 1
 
-/*
 if ("0") {
   alert("Hello");
 } // Yes, it will alert because "0" is a string and it is not empty string. So, it is true.
-*/
 
 // Task - 2
 
-/*
 let name = prompt("What is the “official” name of JavaScript?", "");
 
 if (name == "ECMAScript") {
@@ -135,11 +118,9 @@ if (name == "ECMAScript") {
 } else {
   alert("You don’t know? ECMAScript!");
 }
-*/
 
 // Task - 3 : Show the sign
 
-/*
 let number = prompt("Enter a Number", "");
 
 if (number > 0) {
@@ -149,14 +130,11 @@ if (number > 0) {
 } else {
   alert("0");
 }
-*/
 
 // Task - 4 : Rewrite 'if' into '?'
 
-/*
 let result = 20 % 2 == 0 ? "Even" : "Odd";
 alert(result);
-*/
 
 // Task - 5 : Rewrite 'if..else' into '?'
 
