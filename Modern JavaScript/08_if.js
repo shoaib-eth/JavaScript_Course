a4a33587d385c3d5f631adb7a4d094aa341b2ca9
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-21 16:56:36 +0530

    Initial commit

--- Modern JavaScript/08_if.js
+++ Modern JavaScript/08_if.js
@@ -9,3 +9,31 @@ if (year == 2015) {
   alert("You are right!");
   alert("You are really smart!");
 }
+
+// Example - 2
+
+let year2 = prompt(
+  "In which year was ECMAScript-2015 specification published?",
+  ""
+);
+
+if (year2 == 2015) {
+  alert("You are right!");
+} else {
+  alert("How can you be so wrong?");
+}
+
+// Example - 3
+
+let year3 = prompt(
+  "In which year was ECMAScript-2015 specification published?",
+  ""
+);
+
+if (year3 > 2015) {
+  alert("Too early...");
+} else if (year3 < 2015) {
+  alert("Too Late...");
+} else {
+  alert("Exactly!");
+}
