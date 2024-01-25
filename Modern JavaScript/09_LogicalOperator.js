679fb65cb68c0894d2ea3d9411770c5a462a1157
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-25 16:42:44 +0530

    Initial commit

--- Modern JavaScript/09_LogicalOperator.js
+++ Modern JavaScript/09_LogicalOperator.js
@@ -58,3 +58,149 @@ if (hour == 12 && minute == 30) {
 */
 
 // NOT `!` Operator
+
+/*
+alert(!true); // false
+alert(!0); // true
+*/
+
+// NOT `!` Operator with if
+
+/*
+alert(!"non-empty string"); // false
+alert(!null); // true
+*/
+
+// NOT `!` Operator with double NOT `!!`
+
+/*
+alert(!!"non-empty string"); // true
+alert(!!null); // false
+*/
+
+// Task 1
+
+/*
+alert(null || 2 || undefined); // 2
+*/
+
+// Task 2
+
+/*
+alert(alert(1) || 2 || alert(3)); // 1, 2
+*/
+
+// Task 3
+
+/*
+alert(1 && null && 2); // null
+*/
+
+// Task 4
+
+/*
+
+alert(alert(1) && alert(2)); // 1, undefined
+
+*/
+
+// Task 5
+
+/*
+
+alert(null || (2 && 3) || 4); // 3
+
+*/
+
+// Task 6
+
+/*
+
+let age = prompt("What is your age?", 18);
+
+if (age >= 14 && age <= 90) {
+
+  alert("Your age is between 14 and 90");
+
+}
+
+*/
+
+// Task 7
+
+/*
+
+let age = prompt("What is your age?", 18);
+
+if (!(age >= 14 && age <= 90)) {
+
+  alert("Your age is not between 14 and 90");
+
+}
+
+*/
+
+// Task 8
+
+/*
+
+let age = prompt("What is your age?", 18);
+
+if (age < 14 || age > 90) {
+
+  alert("Your age is not between 14 and 90");
+
+}
+
+*/
+
+// Task 9
+
+/*
+
+
+if (-1 || 0) alert( 'first' ); // first
+
+if (-1 && 0) alert( 'second' ); // nothing
+
+if (null || -1 && 1) alert( 'third' ); // third
+
+*/
+
+// Task 10
+
+/*
+
+let userName = prompt("Who's there?", '');
+
+if (userName == 'Admin') {
+
+  let pass = prompt('Password?', '');
+
+  if (pass == 'TheMaster') {
+
+    alert( 'Welcome!' );
+
+  } else if (pass == '' || pass == null) {
+
+    alert( 'Canceled' );
+
+  } else {
+
+    alert( 'Wrong password' );
+
+  }
+
+}
+
+else if (userName == '' || userName == null) {
+
+  alert( 'Canceled' );
+
+} else {
+
+  alert( "I don't know you" );
+
+}
+
+*/
