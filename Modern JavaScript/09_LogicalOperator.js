341b48be78a4d4daf7f806c63fcec4aa08741209
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-25 16:43:33 +0530

    Initial commit

--- Modern JavaScript/09_LogicalOperator.js
+++ Modern JavaScript/09_LogicalOperator.js
@@ -169,38 +169,20 @@ if (null || -1 && 1) alert( 'third' ); // third
 
 // Task 10
 
-/*
+let userName = prompt("Who's there?", "");
 
-let userName = prompt("Who's there?", '');
+if (userName == "Admin") {
+  let pass = prompt("Password?", "");
 
-if (userName == 'Admin') {
-
-  let pass = prompt('Password?', '');
-
-  if (pass == 'TheMaster') {
-
-    alert( 'Welcome!' );
-
-  } else if (pass == '' || pass == null) {
-
-    alert( 'Canceled' );
-
+  if (pass == "TheMaster") {
+    alert("Welcome!");
+  } else if (pass == "" || pass == null) {
+    alert("Canceled");
   } else {
-
-    alert( 'Wrong password' );
-
+    alert("Wrong password");
   }
-
-}
-
-else if (userName == '' || userName == null) {
-
-  alert( 'Canceled' );
-
+} else if (userName == "" || userName == null) {
+  alert("Canceled");
 } else {
-
-  alert( "I don't know you" );
-
+  alert("I don't know you");
 }
-
-*/
