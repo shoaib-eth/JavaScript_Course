8c05402212dae1e06b6010e0c9ed86d65e0dbc14
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-01-30 09:30:00 +0530

    Initial commit

--- Modern JavaScript/10_Nullishcoalescing.js
+++ Modern JavaScript/10_Nullishcoalescing.js
@@ -6,6 +6,15 @@ alert(user ?? "Anonymous"); // Anonymous
 */
 
 // Example - 2
+/*
 let user = "John";
 
 alert(user ?? "Anonymous"); // John
+*/
+
+// Example - 3
+let firstName = null;
+let lastName = null;
+let nickName = "SuperCoder";
+
+alert(firstName ?? lastName ?? nickName ?? "Anonymous"); // SuperCoder
