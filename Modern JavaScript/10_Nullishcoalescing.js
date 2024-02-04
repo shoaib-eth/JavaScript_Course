b5ae81ea69681124926fd2179a36d358430ac198
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-02-04 17:20:22 +0530

    Initial commit

--- Modern JavaScript/10_Nullishcoalescing.js
+++ Modern JavaScript/10_Nullishcoalescing.js
@@ -31,3 +31,51 @@ let lastName = null;
 let nickName = "SuperCoder";
 
 alert(firstName || lastName || nickName || "Anonymous"); // SuperCoder
+
+// Example - 2
+
+let height = 0;
+
+alert(height || 100); // 100
+
+alert(height ?? 100); // 0
+
+// Example - 3
+
+let height = null;
+
+alert(height || 100); // 100
+
+alert(height ?? 100); // 100
+
+// Example - 4
+
+let height = 0;
+
+alert(height || 100); // 100
+
+alert(height ?? 100); // 0
+
+// Example - 5
+
+let height = null;
+
+alert(height || 100); // 100
+
+alert(height ?? 100); // 100
+
+// Example - 6
+
+let height = 0;
+
+alert(height || 100); // 100
+
+alert(height ?? 100); // 0
+
+// Example - 7
+
+let height = null;
+
+alert(height || 100); // 100
+
+alert(height ?? 100); // 100
