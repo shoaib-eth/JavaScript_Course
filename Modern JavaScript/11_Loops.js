ba39e64450a157e91457cf7224ca2780d80168b2
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-02-04 17:17:53 +0530

    Initial commit

--- Modern JavaScript/11_Loops.js
+++ Modern JavaScript/11_Loops.js
@@ -55,3 +55,18 @@ for (let i = 0; i < 10; i++) {
 }
 
 // Labels for break/continue
+
+outer: for (let i = 0; i < 3; i++) {
+
+  for (let j = 0; j < 3; j++) {
+
+    let input = prompt(`Value at coords (${i},${j})`, '');
+
+    // if an empty string or canceled, then break out of both loops
+    if (!input) break outer; // (*)
+
+    // do something with the value...
+  }
+}
+alert('Done!');
+
