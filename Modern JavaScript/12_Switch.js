f9e1d20845ad148d43f93f96085595cbdf56a79b
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-02-20 11:03:13 +0530

    Initial commit

---
+++ Modern JavaScript/12_Switch.js
@@ -0,0 +1,13 @@
+// Example - switch() statement without break
+let a = 2 + 2;
+
+switch (a) {
+  case 3:
+    alert("Too Small");
+  case 4:
+    alert("Exactly!");
+  case 5:
+    alert("Too Large");
+  case 6:
+    alert("I don't know such value");
+}
