47db796ce9811af9b676962e621ab1592b5cb63c
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-02-21 08:57:22 +0530

    Example - 5

--- Modern JavaScript/13_Functions.js
+++ Modern JavaScript/13_Functions.js
@@ -5,3 +5,5 @@ function showMessage(from, text) {
 }
 
 showMessage(prompt("Enter your name"), prompt("Enter your message"));
+
+// Parameter with prompt function is used to take input from user and then it is passed to the function as an argument.
