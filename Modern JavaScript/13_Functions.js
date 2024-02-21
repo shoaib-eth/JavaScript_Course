e157082a82c7e724a4aaff135cb7f5acbf74352a
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-02-21 09:21:00 +0530

    Example - 7.1

--- Modern JavaScript/13_Functions.js
+++ Modern JavaScript/13_Functions.js
@@ -1,8 +1,12 @@
 // returning a value from a function
 
-function Modulus(a, b) {
-  return a % b;
+function checkAge(age) {
+  if (age >= 18) {
+    return "Access Greanted";
+  } else {
+    return "Access Denied";
+  }
 }
 
-let result = Modulus(10, 3);
-alert(result); // 1
+let result = checkAge(prompt("How old are you?"));
+alert(result);
