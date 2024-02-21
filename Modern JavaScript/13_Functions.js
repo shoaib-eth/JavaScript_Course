7632d9fa01a700a062bbed12df33db0b1c1ca174
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-02-21 08:55:16 +0530

    Example - 5 : Parameters (with  message

--- Modern JavaScript/13_Functions.js
+++ Modern JavaScript/13_Functions.js
@@ -4,10 +4,4 @@ function showMessage(from, text) {
   alert(from + " : " + text);
 }
 
-showMessage("Shoaib", "Hello");
-showMessage("Shoaib", "How are you?");
-showMessage("Shoaib", "What are you doing?");
-showMessage("Shoaib", "Where are you?");
-showMessage("Shoaib", "Why are you not replying?");
-showMessage("Shoaib", "I am waiting for your reply");
-showMessage("Shoaib", "Please reply");
+showMessage(prompt("Enter your name"), prompt("Enter your message"));
