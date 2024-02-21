ae93c6e5837564c64e242ad935dbf7c7eab3ee06
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-02-21 08:45:42 +0530

    Example - 3

--- Modern JavaScript/13_Functions.js
+++ Modern JavaScript/13_Functions.js
@@ -1,7 +1,7 @@
 // Outer Variable
 // A function can access an outer variable as well, for example:
 
-let userName = "Shoaib";
+let userName = prompt("Enter your name: ");
 
 function sayHello() {
   let message = "Hello";
