4c06e10cc59f40e605273b6b9a7033817b831b56
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-02-22 17:19:23 +0530

    Example - 2.1

--- Modern JavaScript/14_FunctionExpressions.js
+++ Modern JavaScript/14_FunctionExpressions.js
@@ -1,21 +1,17 @@
 // Callback function
 function ask(question, yes, no) {
-  if (confirm(question)) {
-    yes();
-  } else {
-    no();
+  if (confirm(question)) yes();
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  function showOk() {
+    alert("You agreed.");
+  },
+  function showCancel() {
+    alert("You canceled the execution.");
   }
-}
-
-function showOk() {
-  alert("You agreed.");
-}
-
-function showCancel() {
-  alert("You canceled the execution.");
-}
-
-// usage: functions showOk, showCancel are passed as arguments to ask function 
-ask("Do you agree?", showOk, showCancel);
+);
 // Do you agree? -> ok -> You agreed.
 // Do you agree? -> cancel -> You canceled the execution.
