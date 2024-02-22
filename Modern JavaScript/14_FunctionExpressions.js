e7fa3f941a24bb8f76d4ac3b5e4bf796af56fbe9
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-02-22 17:14:28 +0530

    Example - 2

--- Modern JavaScript/14_FunctionExpressions.js
+++ Modern JavaScript/14_FunctionExpressions.js
@@ -15,6 +15,7 @@ function showCancel() {
   alert("You canceled the execution.");
 }
 
+// usage: functions showOk, showCancel are passed as arguments to ask function 
 ask("Do you agree?", showOk, showCancel);
 // Do you agree? -> ok -> You agreed.
 // Do you agree? -> cancel -> You canceled the execution.
