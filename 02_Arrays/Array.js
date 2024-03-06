b0a5ba649cd01563614c9790d349f3d634dcb13d
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-03-06 17:12:15 +0530

    Initial commit

--- 02_Arrays/Array.js
+++ 02_Arrays/Array.js
@@ -1,3 +1,8 @@
 let fruits = ["Apple", "Banana"];
 
 console.log(fruits.length); // 2
+
+// Access an Array item using the index position
+
+let first = fruits[0]; // Apple
+let last = fruits[fruits.length - 1]; // Banana
