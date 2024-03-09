46c42e06810dd737cf8a1735e0b7333d53b35829
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-03-09 16:48:26 +0530

    Initial commit

--- 02_Arrays/ArrayPushPop.js
+++ 02_Arrays/ArrayPushPop.js
@@ -21,13 +21,11 @@ const fruits3 = ["apple", "banana", "orange"];
 fruits3.unshift("kiwi");
 console.log(fruits3); // Output: ['kiwi', 'apple', 'banana', 'orange']
 
-
 // Example 5
 
 const fruits4 = ["apple", "banana", "orange"];
 const pos = fruits4.indexOf("banana");
 console.log(pos); // Output: 1
-
 
 // Example 6
 
@@ -35,22 +33,19 @@ const fruits5 = ["apple", "banana", "orange"];
 const removedItem = fruits5.splice(1, 1);
 console.log(fruits5); // Output: ['apple', 'orange']
 
-
 // Example 7
 
 const vegetables = ["cabbage", "turnip", "radish", "carrot"];
 const pos1 = 1;
 const n = 2;
-const removedItems = vegetables.splice(pos1, n); 
+const removedItems = vegetables.splice(pos1, n);
 console.log(vegetables); // Output: ['cabbage', 'carrot']
-
 
 // Example 8
 
 const fruits6 = ["apple", "banana", "orange"];
 const shallowCopy = fruits6.slice();
 console.log(shallowCopy); // Output: ['apple', 'banana', 'orange']
-
 
 // Example 9
 
@@ -63,5 +58,3 @@ console.log(shallowCopy1); // Output: ['banana', 'orange']
 const fruits8 = ["apple", "banana", "orange"];
 const shallowCopy2 = fruits8.slice(1);
 console.log(shallowCopy2); // Output: ['banana', 'orange']
-
-
