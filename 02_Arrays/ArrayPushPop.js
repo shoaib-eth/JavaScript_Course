2d180b160c225b94abdc7c38b356e323f1c47cdf
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-03-09 16:42:05 +0530

    Initial commit

--- 02_Arrays/ArrayPushPop.js
+++ 02_Arrays/ArrayPushPop.js
@@ -8,3 +8,10 @@ console.log(fruits); // Output: ['apple', 'banana', 'orange', 'grape', 'kiwi']
 const fruits1 = ["apple", "banana", "orange"];
 fruits1.pop();
 console.log(fruits1); // Output: ['apple', 'banana']
+
+// Example 3
+
+const fruits2 = ["apple", "banana", "orange"];
+fruits2.shift();
+console.log(fruits2); // Output: ['banana', 'orange']
+
