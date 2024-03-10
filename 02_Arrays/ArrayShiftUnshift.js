d6b53bd8bdd61d0cf5a3c9e81dbb5334a4f5d2c1
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-03-10 16:36:54 +0530

    Initial commit

--- 02_Arrays/ArrayShiftUnshift.js
+++ 02_Arrays/ArrayShiftUnshift.js
@@ -4,3 +4,10 @@ var arr = [1, 2, 3, 4, 5];
 arr.shift(); // 1
 
 console.log(arr); // [2, 3, 4, 5]
+
+// Example 2
+
+var arr = [1, 2, 3, 4, 5];
+arr.unshift(0); // 6
+
+console.log(arr); // [0, 1, 2, 3, 4, 5]
