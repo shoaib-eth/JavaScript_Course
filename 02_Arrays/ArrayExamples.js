6d7e5c2233e0a8aa082724144f849c13bc5a8d48
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-03-13 13:26:31 +0530

    Initial commit

--- 02_Arrays/ArrayExamples.js
+++ 02_Arrays/ArrayExamples.js
@@ -5,3 +5,19 @@ let numbers = [1, 2, 3, 4, 5];
 
 // Print the array
 console.log(numbers); // Output: [1, 2, 3, 4, 5]
+
+// Example 2
+
+// Create an array of strings
+let colors = ['red', 'yellow', 'green', 'blue'];
+
+// Print the array
+console.log(colors); // Output: ['red', 'yellow', 'green', 'blue']
+
+// Example 3
+
+// Create an array of mixed data types
+let random = [1, 'red', 3, 'blue'];
+
+// Print the array
+console.log(random); // Output: [1, 'red', 3, 'blue']
