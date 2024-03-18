b0e749db349a555b2525ad328be3193f1b85db25
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-03-18 15:33:53 +0530

    Initial commit

--- 03_Objects/Objects.js
+++ 03_Objects/Objects.js
@@ -11,3 +11,23 @@ var person = {
 // Display some data from the object:
 document.getElementById("demo").innerHTML =
   person.firstName + " is " + person.age + " years old.";
+
+// Example 2
+
+// Create an object:
+var person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue",
+};
+
+// Change a property:
+person.age = 40;
+
+// Add a property:
+person.national;
+
+// Display some data from the object:
+document.getElementById("demo").innerHTML =
+  person.firstName + " is " + person.age + " years old.";
