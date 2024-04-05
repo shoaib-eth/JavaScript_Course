da4a23309350e4b75e1cdd5cc783ccf3a5fdf888
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-04-05 16:18:17 +0530

    Add JavaScript data types and examples in 02_dataTypes.js

---
+++ JavaScript/01_Basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // This is used to enable strict mode in JavaScript and treat all JS code as newer version.
+
+alert("Data Types in JavaScript"); // This will show an alert box with the message "Data Types in JavaScript".
+
+console.log(3 
+    + 
+    3); // code readability should be high
+
+console.log("Shoaib");
+
+let name = "Shoaib"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// Data Types in JavaScript
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
