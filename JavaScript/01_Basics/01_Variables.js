d12aa8a00179a703b7d513419fdd550fd6fa4b5f
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-04-05 16:14:09 +0530

    Add JavaScript variables and their values in 01_Variables.js

---
+++ JavaScript/01_Basics/01_Variables.js
@@ -0,0 +1,24 @@
+const userId = 29240;
+let userEmail = "shoaib@google.com";
+var userPassword = "Shoaib@123";
+userCity = "Gwalior";
+let userState;
+
+/*
+userId = 12345; // Error: Assignment to constant variable. 
+This is not allowed because userId is a constant variable and its value can't be changed.
+*/ 
+
+// Values can be changed for let and var variables.
+userEmail = "shoaib@gmail.com";
+userPassword = "6872";
+userCity = "Indore";
+userState = "Madhya Pradesh";
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+// Printing the values of the variables.
+console.table({ userId, userEmail, userPassword, userCity, userState });
