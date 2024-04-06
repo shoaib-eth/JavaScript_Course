e21ad2be8f2640e820d01a269807043503a79000
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-04-06 15:04:09 +0530

    Fix syntax errors and add missing semicolons in 03_ConversionOperation.js

--- JavaScript/01_Basics/03_ConversionOperation.js
+++ JavaScript/01_Basics/03_ConversionOperation.js
@@ -1,36 +1,35 @@
-let score = "Shoaib"
+let score = "Shoaib";
 
 //console.log(typeof score); // string
 //console.log(typeof(score)); // string
 
-let valueInNumber = Number(score)
+let valueInNumber = Number(score);
 //console.log(typeof valueInNumber); // number
 //console.log(valueInNumber); // NaN
-
 
 // "33" => 33
 // "33abc" => NaN
 //  true => 1; false => 0
 
-let isLoggedIn = "Shoaib"
+let isLoggedIn = "Shoaib";
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+let booleanIsLoggedIn = Boolean(isLoggedIn);
 //  console.log(booleanIsLoggedIn); // true
 
 // 1 => true; 0 => false
 // "" => false
 // "Shoaib" => true
 
-let someNumber = 33
+let someNumber = 33;
 
-let stringNumber = String(someNumber)
+let stringNumber = String(someNumber);
 // console.log(stringNumber); // "33"
 // console.log(typeof stringNumber); // string
 
 // *********************** Operations ***********************
 
-let value = 3
-let negValue = -value
+let value = 3;
+let negValue = -value;
 // console.log(negValue); // -3
 
 // console.log(2+2);
@@ -40,29 +39,29 @@ let negValue = -value
 // console.log(2/3);
 // console.log(2%3);
 
-let str1 = "hello"
-let str2 = " Shoaib"
+let str1 = "hello";
+let str2 = " Shoaib";
 
-let str3 = str1 + str2
+let str3 = str1 + str2;
 // console.log(str3); // hello Shoaib
 
-// console.log("1" + 2); // 12 
+// console.log("1" + 2); // 12
 // console.log(1 + "2"); // 12
-// console.log("1" + 2 + 2); // 122 
+// console.log("1" + 2 + 2); // 122
 // console.log(1 + 2 + "2"); // 32
 
-// console.log( (3 + 4) * 5 % 3); // 0 
+// console.log( (3 + 4) * 5 % 3); // 0
 
 // console.log(+true); // 1
 // console.log(+""); // 0
 
-let num1, num2, num3
+let num1, num2, num3;
 
-num1 = num2 = num3 = 2 + 2
+num1 = num2 = num3 = 2 + 2;
 
-let gameCounter = 100
+let gameCounter = 100;
 ++gameCounter;
-console.log(gameCounter); // 101  
+console.log(gameCounter); // 101
 
 // pre increment
 // link to study
