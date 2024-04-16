c0610ecabc34478b5928425716fd3cbf25a0426b
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-04-16 15:35:50 +0530

    Inital commit

--- JavaScript/01_Basics/06_nums_and_maths.js
+++ JavaScript/01_Basics/06_nums_and_maths.js
@@ -1,34 +1,34 @@
-const score = 400
-// console.log(score);
+const score = 400;
+// console.log(score); // 400
 
-const balance = new Number(100)
-// console.log(balance);
+const balance = new Number(100);
+// console.log(balance); // 100
 
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(1));
+// console.log(balance.toString().length); // 3
+// console.log(balance.toFixed(1)); // 100.0
 
-const otherNumber = 123.8966
+const otherNumber = 123.8966;
 
-// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(4)); // 123.9
 
-const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN')); // 10,00,000
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
-// console.log(Math);
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.6));
-// console.log(Math.ceil(4.2));
-// console.log(Math.floor(4.9));
-// console.log(Math.min(4, 3, 6, 8));
-// console.log(Math.max(4, 3, 6, 8));
+// console.log(Math); // Math object
+// console.log(Math.abs(-4)); // 4
+// console.log(Math.round(4.6)); // 5
+// console.log(Math.ceil(4.2)); // 5
+// console.log(Math.floor(4.9));  // 4
+// console.log(Math.min(4, 3, 6, 8)); // 3
+// console.log(Math.max(4, 3, 6, 8)); // 8
 
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
+console.log(Math.random()); // random number between 0 and 1
+console.log(Math.random() * 10 + 1); // random number between 1 and 10
+console.log(Math.floor(Math.random() * 10) + 1); // random number between 1 and 10
 
-const min = 10
-const max = 20
+const min = 10;
+const max = 20;
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
