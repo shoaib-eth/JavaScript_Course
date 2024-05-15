c95198624e626e80552f88ac3cdfb5cde411a91a
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-05-15 19:19:03 +0530

    Initial commit

--- 05_Important Array Methods/08_some-method.js
+++ 05_Important Array Methods/08_some-method.js
@@ -2,7 +2,7 @@
 const numbers = [3, 5, 6, 9];
 
 const isEven = numbers.some((number) => number % 2 === 0);
-console.log(isEven);
+console.log(isEven); // Output - > true
 
 // Example - > 2
 const cartItems = [
@@ -12,16 +12,28 @@ const cartItems = [
 ];
 
 const myCart = cartItems.some((product) => product.price <= 50000);
-console.log(myCart);
+console.log(myCart); // Output - > true
 
 // Example - > 3
 const names = ["Alice", "Bob", "Charlie", "David"];
 
 const ans = names.some((name) => name.length >= 5);
-console.log(ans);
+console.log(ans); // Output - > true
 
 // Example - > 4
 const grades = [80, 85, 90, 75, 95];
 
 const result = grades.some((grade) => grade >= 90);
-console.log(result);
+console.log(result);  // Output - > true
+
+// Example - > 5
+
+const students = [
+  { name: "Alice", age: 20 },
+  { name: "Bob", age: 22 },
+  { name: "Charlie", age: 25 },
+  { name: "David", age: 18 },
+];
+
+const isAdult = students.some((student) => student.age >= 18);
+console.log(isAdult); // Output - > true
