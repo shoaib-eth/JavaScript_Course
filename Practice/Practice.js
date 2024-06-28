d6c5cc36fe42d1ba20bff1baea91df09a1701c5e
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-06-28 16:26:30 +0530

    Initial commit

--- Practice/Practice.js
+++ Practice/Practice.js
@@ -1,23 +1,30 @@
-let admin;
-let name;
+const readline = require("readline");
 
-name = "John";
-admin = name;
+console.log("Hello, world!");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-console.log(admin);
-
-// Example - 2 : Naming correct variable names
-let ourPlanetName = "Earth";
-let currentUserName = "John";
-
-console.log(`Our Planest Name is ${ourPlanetName}`);
-console.log(`Current User Name is ${currentUserName}`);
-
-// Example - 3 : Uppercase constants
-
-const BIRTHDAY = "18.04.1982"; // make uppercase?
-
-const AGE = someCode(BIRTHDAY); // make uppercase?
-
-console.log(`My Birthday is ${BIRTHDAY}`);
-console.log(`My Age is ${AGE}`);
+rl.question("Enter your name: ", (name) => {
+  rl.question("Enter your course: ", (course) => {
+    rl.question("Enter your email: ", (email) => {
+      rl.question("Enter your address: ", (address) => {
+        rl.question("Enter your college name: ", (college) => {
+          rl.question("Enter your CGPA: ", (cgpa) => {
+            console.log("Details:");
+            console.log("----------------------------------");
+            console.log("| Name     |", name);
+            console.log("| Course   |", course);
+            console.log("| Email    |", email);
+            console.log("| Address  |", address);
+            console.log("| College  |", college);
+            console.log("| CGPA     |", cgpa);
+            console.log("----------------------------------");
+            rl.close();
+          });
+        });
+      });
+    });
+  });
+});
