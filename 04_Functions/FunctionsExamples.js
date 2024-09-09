02d7ad6d87ece973854cd5cc0962102799759a4b
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-09-09 17:59:38 +0530

    Initial commit

--- 04_Functions/FunctionsExamples.js
+++ 04_Functions/FunctionsExamples.js
@@ -1,68 +1,68 @@
 function calculateAverage(numbers) {
-    if (!Array.isArray(numbers)) {
-        throw new Error('Input must be an array');
-    }
+  if (!Array.isArray(numbers)) {
+    throw new Error("Input must be an array");
+  }
 
-    if (numbers.length === 0) {
-        throw new Error('Array must not be empty');
-    }
+  if (numbers.length === 0) {
+    throw new Error("Array must not be empty");
+  }
 
-    let sum = 0;
-    for (let i = 0; i < numbers.length; i++) {
-        if (typeof numbers[i] !== 'number') {
-            throw new Error('Array must contain only numbers');
-        }
-        sum += numbers[i];
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    if (typeof numbers[i] !== "number") {
+      throw new Error("Array must contain only numbers");
     }
+    sum += numbers[i];
+  }
 
-    return sum / numbers.length;
+  return sum / numbers.length;
 }
 
 function findMax(numbers) {
-    if (!Array.isArray(numbers)) {
-        throw new Error('Input must be an array');
-    }
+  if (!Array.isArray(numbers)) {
+    throw new Error("Input must be an array");
+  }
 
-    if (numbers.length === 0) {
-        throw new Error('Array must not be empty');
-    }
+  if (numbers.length === 0) {
+    throw new Error("Array must not be empty");
+  }
 
-    let max = numbers[0];
-    for (let i = 1; i < numbers.length; i++) {
-        if (typeof numbers[i] !== 'number') {
-            throw new Error('Array must contain only numbers');
-        }
-        if (numbers[i] > max) {
-            max = numbers[i];
-        }
+  let max = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    if (typeof numbers[i] !== "number") {
+      throw new Error("Array must contain only numbers");
     }
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+  }
 
-    return max;
+  return max;
 }
 
 function isPalindrome(word) {
-    if (typeof word !== 'string') {
-        throw new Error('Input must be a string');
-    }
+  if (typeof word !== "string") {
+    throw new Error("Input must be a string");
+  }
 
-    const reversedWord = word.split('').reverse().join('');
-    return word === reversedWord;
+  const reversedWord = word.split("").reverse().join("");
+  return word === reversedWord;
 }
 
 function isPrime(number) {
-    if (typeof number !== 'number') {
-        throw new Error('Input must be a number');
-    }
+  if (typeof number !== "number") {
+    throw new Error("Input must be a number");
+  }
 
-    if (number <= 1) {
-        return false;
-    }
+  if (number <= 1) {
+    return false;
+  }
 
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-        if (number % i === 0) {
-            return false;
-        }
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false;
     }
+  }
 
-    return true;
+  return true;
 }
