c95ad8520d0ab87b08c7956686e60a842d626065
ShoaibDev69 <shoaib.developer69@gmail.com>
2025-01-01 13:20:08 +0530

    Initial commit

--- Random JS Code/NewYear2025.js
+++ Random JS Code/NewYear2025.js
@@ -21,3 +21,13 @@ function newYearWishes() {
 
 // Call the function to execute
 newYearWishes();
+
+// Output:
+// Happy New Year 2025, John! Wishing you a year full of joy and success!
+// or
+// Happy New Year 2025! Wishing you a year full of joy and success!
+
+// Explanation:
+// This JavaScript code snippet defines a function called newYearWishes that prompts the user to enter their name and then displays a New Year message with the user's name. If the user enters a name, the message includes the user's name; otherwise, it displays a generic message. The function is then called to execute the code.
+
+// The prompt function is used to get user input, and the console.log function is used to display the New Year wishes message. The function newYearWishes is defined to encapsulate the logic for displaying the message, making the code more modular and reusable.
